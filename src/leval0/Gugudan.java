eb73dc2c1868e0978d4ca04d8cba6b2f5e0c105c
ej31 <devil1032@gmail.com>
2023-05-02 03:41:39 +0900

    Initialize

---
+++ src/leval0/Gugudan.java
@@ -0,0 +1,12 @@
+package leval0;
+
+public class Gugudan {
+	public static void main(String[] args) {
+
+		// 유저에게 숫자를 입력 받으면 구구단을 출력하는 코드를 개발하세요
+		// - Optional : 유저의 입력이 의도와 다른 값이 들어올 경우 예외 처리 할 수 있습니다.
+		// - Optional : 유저의 입력을 받고 출력이 끝난 후에 다시 숫자를 입력 받을 수 있도록 구성하여 프로그램이 끝나지 않도록 합니다.
+		
+		
+	}
+}
---
+++ src/leval0/ReverseTriangleUserInput.java
@@ -0,0 +1,11 @@
+package leval0;
+
+public class ReverseTriangleUserInput {
+	public static void main(String[] args) {
+		// 유저에게 숫자를 입력받습니다.
+		// 그 숫자의 크기만큼의 높이를 가지는 역직각삼각형을 출력합니다.
+		// - Optional : 유저의 입력이 의도와 다른 값이 들어올 경우 예외 처리 할 수 있습니다.
+		// - Optional : 유저의 입력을 받고 출력이 끝난 후에 다시 숫자를 입력 받을 수 있도록 구성하여 프로그램이 끝나지 않도록 합니다.
+
+	}
+}
